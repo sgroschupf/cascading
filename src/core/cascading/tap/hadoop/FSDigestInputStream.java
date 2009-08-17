388ef804233b3d20d22579705fae468372cd4d5a
Peter Voss <info@petervoss.org>
2009-08-17 08:40:16 +0200

    Applied Chris' fix of Cascading wip-1.0.15 d1f9a99809c67ca2d57aade4ff41c998c3c4ff2a. 'Fixed issue in c.t.h.FSDigestOutputStream where seek() now must be implemented with modern versions of Hadoop.'

--- src/core/cascading/tap/hadoop/FSDigestInputStream.java
+++ src/core/cascading/tap/hadoop/FSDigestInputStream.java
@@ -120,9 +120,13 @@ public class FSDigestInputStream extends FSInputStream
     }
 
   @Override
-  public void seek( long pos ) throws IOException
-    {
-    throw new IOException( "not supported" );
+  public void seek(long pos) throws IOException {
+        if (getPos() == pos)
+            return;
+        if (getPos() > pos)
+            throw new IOException("cannot seek to " + pos + ", currently at" + getPos());
+        int len = (int) (pos - getPos());
+        read(new byte[len], 0, len);
     }
 
   @Override
